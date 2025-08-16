d24d7b68313c912444da4f9a3387e894c5a612c9
nadermkhan <muhammadnadermahbubkhan@gmail.com>
2025-08-16 12:09:28 +0600

    pre-release tags

--- app/src/main/java/nogor/pay/MainActivity.java
+++ app/src/main/java/nogor/pay/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(MainActivity.this, "PreRelease by Nader", Toast.LENGTH_LONG).show();
         initViews();
         setupDatabase();
         checkPermissions();
